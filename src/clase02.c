0c7ef89a9796b9dbdacfd4a9ad4840010ca31529
Juan <juanmrodriguez1989@gmail.com>
2021-08-26 21:41:43 -0300

    primer commit

---
+++ src/clase02.c
@@ -0,0 +1,44 @@
+/*
+ ============================================================================
+ Name        : clase02.c
+ Author      : Juan Manuel
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	setbuf(stdout, NULL);
+
+	int numeroUno;
+	int acumular;
+	char respuesta;
+
+	acumular=0;
+
+	do
+	{
+		printf("Por favor ingrese un numero: ");
+		fflush(stdin);
+		scanf("%d", &numeroUno);
+
+		acumular=acumular+numeroUno;
+
+		printf("Desea continuar? ");
+		fflush(stdin);
+		scanf("%c", &respuesta);
+	} while(respuesta=='s');
+
+
+
+	printf("\nLa suma acumulada es: %d", acumular);
+
+
+
+	return EXIT_SUCCESS;
+}
